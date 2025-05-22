e43581167f7dc1207b7d9fbb3a23da1366e3b97c
Do Quoc Sang <quocsang316@gmail.com>
2025-05-22 16:56:03 +0700

    feat(todo): show todo list

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,27 @@
+import { useState } from "react";
+import TodoItem from "./components/TodoItem"
+import { dummyData } from "./data/todo"
+
 function App() {
+  const [todos, setTodos] = useState(dummyData);
+
+  function handleCompleteChange(id: number, completed: boolean) {
+    setTodos((prevTodos) => prevTodos.map((todo) => 
+        todo.id === id ?  { ...todo, completed } : todo
+    ));
+  }
   return (
-      <h1 className="text-3xl font-bold text-red-500">
-        Hello world!
+    <>
+       <h1 className="text-3xl font-bold text-red-500">
+        Todo List
       </h1>
+      {todos.map((todo) => (
+        <TodoItem 
+          key={todo.id} 
+          todo={todo}
+          onCompleteChange={handleCompleteChange} />
+      ))}
+    </>
   )
 }
 
