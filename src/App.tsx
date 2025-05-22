03454a8fb68f3da0c2412f244a0998f15246af95
Do Quoc Sang <quocsang316@gmail.com>
2025-05-22 17:13:17 +0700

    feat(todo): add toto item

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import TodoItem from "./components/TodoItem"
 import { dummyData } from "./data/todo"
+import AddTodoForm from "./components/AddTodoItemForm";
 
 function App() {
   const [todos, setTodos] = useState(dummyData);
@@ -10,11 +11,24 @@ function App() {
         todo.id === id ?  { ...todo, completed } : todo
     ));
   }
+
+  function handleAddTodo(title: string) {
+    const newTodo = {
+      id: todos.length + 1,
+      title,
+      completed: false,
+    };
+    setTodos((prevTodos) => [...prevTodos, newTodo]);
+  }
+
   return (
     <>
        <h1 className="text-3xl font-bold text-red-500">
         Todo List
       </h1>
+      
+      <AddTodoForm onAddTodo={handleAddTodo}/>
+
       {todos.map((todo) => (
         <TodoItem 
           key={todo.id} 
